d67587c391c124b989c313ffa2fc74088ecab8c1
sarvani <annamrajusarvani@cronj.com>
2015-04-20 23:35:14 +0530

    WebScraper

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,8 @@
 // Load modules
 
 var User      = require('./controller/user'),
-    Static    = require('./static');
+    Static    = require('./static'),
+    scraper      = require('./controller/WebScraper/scraper.js');
 
 // API Server Endpoints
 exports.endpoints = [
@@ -9,5 +10,6 @@ exports.endpoints = [
     { method: 'GET',  path: '/{somethingss*}', config: Static.get },
     { method: 'POST', path: '/user', config: User.create},
     { method: 'POST', path: '/verifyEmail', config: User.verifyEmail},
+    { method: 'POST', path: '/GetData', config: scraper.getdata},
    
 ];
